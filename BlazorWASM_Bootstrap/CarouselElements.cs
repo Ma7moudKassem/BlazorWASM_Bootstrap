00f92839fae49499dada0041463f9531fec6075c
Mahmoud Kassem <modykassem123@gmail.com>
2022-07-15 04:35:40 +0200

    Handiling somethings

---
+++ BlazorWASM_Bootstrap/CarouselElements.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorWASM_Bootstrap
+{
+    public class CarouselElements
+    {
+
+        public string? Images { get; set; }
+        public string? Labels { get; set; }
+        public string? Description { get; set; }
+        public string? Alt { get; set; }
+    }
+}
